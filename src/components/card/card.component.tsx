b805ac532e0c25dd6194589fe9806a3a0d7e1f1f
Lakshmikanta <frontendfixer@gmail.com>
2023-06-26 13:41:04 +0530

    typed card component to <Monster> type

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,6 +1,12 @@
 import './card.style.css';
 
-const Card = ({ monster }) => {
+import { Monster } from '../../App';
+
+type CardProps = {
+  monster: Monster
+}
+
+const Card = ({ monster }:CardProps) => {
   const { name, email, id } = monster;
   return (
     <div className="card-container">
